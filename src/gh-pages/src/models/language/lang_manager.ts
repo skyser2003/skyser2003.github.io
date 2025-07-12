1588790ec8384f61a15da3c577ff1aaf57674d66
Seung Ho Jang <shjang1992@gmail.com>
2025-07-12 13:49:59 +0900

    Change to nextjs & mui

--- src/gh-pages/src/models/language/lang_manager.ts
+++ src/gh-pages/src/models/language/lang_manager.ts
@@ -1,17 +1,19 @@
-import UILangData from "../../assets/lang_data.json";
+import UILangData from "@/data/lang_data.json";
 
 const langCodeRegex = /^(.+)-.+$/;
 
 class LanguageManager {
-  private uiLangData: { [key: string]: { [key: string]: string } };
+    private uiLangData: { [key: string]: { [key: string]: string } };
 
-  constructor(private lang: string) {
-    this.uiLangData = UILangData as { [key: string]: { [key: string]: string } };
-  }
+    constructor(private lang: string) {
+        this.uiLangData = UILangData as {
+            [key: string]: { [key: string]: string };
+        };
+    }
 
-  public getData(key: string) {
-    return this.uiLangData[key][this.lang];
-  }
+    public getData(key: string) {
+        return this.uiLangData[key][this.lang];
+    }
 }
 
 let langCode = "en";
@@ -19,7 +21,7 @@ let langCode = "en";
 const langMatch = window.navigator.language.match(langCodeRegex);
 
 if (langMatch !== null) {
-  langCode = langMatch[1];
+    langCode = langMatch[1];
 }
 
 const lm = new LanguageManager(langCode);
