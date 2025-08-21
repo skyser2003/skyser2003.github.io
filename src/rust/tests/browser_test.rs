ee1f5f68763e2830e5bca0595a4ee460b873f79f
Seung Ho Jang <shjang1992@gmail.com>
2025-08-22 00:51:15 +0900

    Test fix

--- src/rust/tests/browser_test.rs
+++ src/rust/tests/browser_test.rs
@@ -8,13 +8,13 @@ wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 async fn test_wasm_api_download() -> Result<(), JsValue> {
     let downloader = Downloader::new("timinar/baby-llama-58m");
     downloader
-        .save_file("model.safetensors", "model", None)
+        .save_file("model.safetensors", "model")
+        .start()
         .await?;
     downloader
-        .save_file("tokenizer.json", "tokenizer", None)
+        .save_file("tokenizer.json", "tokenizer")
+        .start()
         .await?;
-
-    println!("boo");
 
     Ok(())
 }
